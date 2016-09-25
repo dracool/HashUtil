a0e53fcbcb57ef61c0f84b5094d2c03bda874c96
Mirco Gericke <dracool@users.noreply.github.com>
2016-09-26 00:21:59 +0200

    added license updated assembly info

--- HashUtil/Properties/AssemblyInfo.cs
+++ HashUtil/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HashUtil")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Mirco Gericke 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
