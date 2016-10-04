5fe769ed1a020aec47c7e18c0c2b532f74868fef
Mirco Gericke <dracool@users.noreply.github.com>
2016-10-04 23:33:02 +0200

    added documentation

--- HashUtil/Hashing/Hash.cs
+++ HashUtil/Hashing/Hash.cs
@@ -5,16 +5,27 @@ using System.Linq;
 
 namespace HashUtil.Hashing
 {
+    /// <summary>
+    /// Represents a hash value in byte and string form
+    /// </summary>
     [Serializable]
     public class Hash : IEquatable<Hash>, IEquatable<string>, IEquatable<byte[]>, ICloneable, IEnumerable<byte>, IEnumerable<char>
     {
         #region constructors
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="hash"></param>
         public Hash(string hash)
         {
             Bytes = HashingUtils.HexToBytes(hash);
             Hex = hash;
         }
 
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="hash">the hash to use</param>
         public Hash(byte[] hash)
         {
             Hex = HashingUtils.BytesToHex(hash);
@@ -23,11 +34,19 @@ namespace HashUtil.Hashing
         #endregion
 
         #region operators: conversion
+        /// <summary>
+        /// converts the value to string
+        /// </summary>
+        /// <param name="value"></param>
         public static implicit operator string(Hash value)
         {
             return value?.Hex ?? string.Empty;
         }
 
+        /// <summary>
+        /// converts the value to byte[]
+        /// </summary>
+        /// <param name="value"></param>
         public static implicit operator byte[] (Hash value)
         {
             return value?.Bytes;
@@ -35,12 +54,24 @@ namespace HashUtil.Hashing
         #endregion
 
         #region operators: comparison(Hash)
+        /// <summary>
+        /// compares to <see cref="Hash"/>
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator ==(Hash left, Hash right)
         {
             var val = left?.Equals(right) ?? object.Equals(right, null);
             return val;
         }
 
+        /// <summary>
+        /// compares to <see cref="Hash"/>
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator !=(Hash left, Hash right)
         {
             return !(left == right);
@@ -48,21 +79,45 @@ namespace HashUtil.Hashing
         #endregion
 
         #region operators: comparison(string)
+        /// <summary>
+        /// compares to string
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator ==(Hash left, string right)
         {
             return left?.Equals(right) ?? right == null;
         }
 
+        /// <summary>
+        /// compares to string
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator !=(Hash left, string right)
         {
             return !(left?.Equals(right) ?? right == null);
         }
 
+        /// <summary>
+        /// compares to string
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator ==(string left, Hash right)
         {
             return right?.Equals(left) ?? left == null;
         }
 
+        /// <summary>
+        /// compares to string
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator !=(string left, Hash right)
         {
             return !(right?.Equals(left) ?? left == null);
@@ -70,21 +125,44 @@ namespace HashUtil.Hashing
         #endregion
 
         #region operators: comparison(byte[])
+        /// <summary>
+        /// compares to bytes
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator ==(Hash left, byte[] right)
         {
             return left?.Equals(right) ?? right == null;
         }
 
+        /// <summary>
+        /// compares to bytes
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator !=(Hash left, byte[] right)
         {
             return !(left?.Equals(right) ?? right == null);
         }
 
+        /// <summary>
+        /// compares to bytes
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator ==(byte[] left, Hash right)
         {
             return right?.Equals(left) ?? left == null;
         }
-
+        /// <summary>
+        /// compares to bytes
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator !=(byte[] left, Hash right)
         {
             return !(right?.Equals(left) ?? left == null);
@@ -92,40 +170,81 @@ namespace HashUtil.Hashing
         #endregion
 
         #region properties
+        
+        /// <summary>
+        /// representation of the hash as lowercase string
+        /// </summary>
         public string Hex { get; }
+        
+        /// <summary>
+        /// representation of the hash as bytes
+        /// </summary>
         public byte[] Bytes { get; }
 
+        /// <summary>
+        /// Length of the hash in bit
+        /// </summary>
         public int Length => Bytes.Length * 8;
         #endregion
 
         #region Object overrides
+        /// <summary>
+        /// returns the string representation
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return Hex;
         }
 
+        /// <summary>
+        /// returns the hash representation
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             return Hex.GetHashCode();
         }
 
+        /// <summary>
+        /// equality comparison
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
-            return Equals(obj as Hash);
+            return Equals(obj as Hash) 
+                || Equals(obj as string) 
+                || Equals(obj as byte[]);
         }
         #endregion
 
         #region IEquatable<*>
+        /// <summary>
+        /// Compares the equality of this hash with another
+        /// </summary>
+        /// <param name="other">the Hash to compare to</param>
+        /// <returns>true if the hashes are equal, false otherwise</returns>
         public bool Equals(Hash other)
         {
             return Equals(other?.Bytes);
         }
 
+        /// <summary>
+        /// Compares the equality of string representation with another string
+        /// </summary>
+        /// <param name="other">the string to compare to</param>
+        /// <returns>true if the strings are equal, false otherwise</returns>
         public bool Equals(string other)
         {
-            return Hex?.Equals(other) ?? false;
+            return Hex?.Equals(other, StringComparison.OrdinalIgnoreCase) ?? false;
         }
 
+        /// <summary>
+        /// Compares the equality of the bytes with a byte array
+        /// </summary>
+        /// <param name="other">the byte array to compare against</param>
+        /// <returns>true if the byte sequence is equal, false otherwise</returns>
         public bool Equals(byte[] other)
         {
             return other?.SequenceEqual(Bytes) ?? false;
@@ -133,6 +252,10 @@ namespace HashUtil.Hashing
         #endregion
 
         #region ICloneable
+        /// <summary>
+        /// Returns a copy of the hash
+        /// </summary>
+        /// <returns></returns>
         public Hash Clone()
         {
             return new Hash(Bytes);
@@ -145,6 +268,10 @@ namespace HashUtil.Hashing
         #endregion
 
         #region IEnumerable
+        /// <summary>
+        /// Returns a <see cref="IEnumerator{T}"/> that enumerates over the bytes of the hash
+        /// </summary>
+        /// <remarks>contains an explicit definition for char enumerables</remarks>
         public IEnumerator<byte> GetEnumerator()
         {
             return ((IEnumerable<byte>)Bytes).GetEnumerator();
